536acec560bb570e12c3931710bd1af165b7bcd7
Philip Zeisler <wi23b107@technikum-wien.at>
2024-05-29 16:57:27 +0200

    updated main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello TicTacToe!");
+
+        System.out.println("Sebastian will Heim");
     }
 }
